733ec58b1c9624599b2c23c1e18893541111db9e
Shubha737 <86780368+Shubha737@users.noreply.github.com>
2021-08-27 22:07:12 +0530

    Remove a key from dictionary

---
+++ Day_13_Remove_a_key_from_dictionary.py
@@ -0,0 +1,13 @@
+# Day 13 code 2
+# To remove a key from dictionary
+
+Student_dict_1 = {'Name':'Shubham Kumar Gupta','Age':'26','Qualification':'Master In Engineering','Company':'Cognizant'}
+Student_dict_2 = {'Future Ambition':'Software Developer','Method':'Python and DSA learning'} 
+
+Student_dict_1.pop('Name')
+del Student_dict_1['Age']
+
+# .pop() or del method can be used to delete a particular key from a list.
+
+
+print(Student_dict_1)
