e2feaffad3b5f69cace2da5522eaf720e2c2df6c
Shubham Kumar <gupta1995shubh@gmail.com>
2021-09-20 16:51:11 +0000

    max and second_max num

--- max_num_in_list.py
+++ max_num_in_list.py
@@ -1,11 +1,18 @@
-# to find largest no. in a list.
+# to find second largest no. in a list.
 
-num_list = [55,54,75,3,90,56,1,9054,0,98]
+num_list = [5,7,8,9,9,8]
 maxi = num_list[1]
+sec_maxi = num_list[0]
 for i in num_list :
-    if (i>=maxi):
+    if (i>maxi):
+        sec_maxi = maxi
         maxi = i
-    else :
+    elif (i<maxi and maxi!=sec_maxi) :
+        if (i>sec_maxi and sec_maxi<maxi and i!=maxi):
+            sec_maxi = i
+
+    elif (i==maxi and maxi!=sec_maxi) :
         continue
     
 print(f'The Maximum is {maxi}')
+print(sec_maxi)
